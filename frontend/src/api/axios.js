ab89404d362a286c9dbf89b1fea3615d8ab297f3
Arpita Rai <raiarpita58@gmail.com>
2025-08-03 20:21:54 +0530

    fixed cors issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
-
+  import.meta.env.VITE_API_BASE_URL ;
 const instance = axios.create({
   baseURL,
   withCredentials: true,
