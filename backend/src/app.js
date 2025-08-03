355188ba3ed91b3822625ffa85e73c56cc533390
Arpita Rai <raiarpita58@gmail.com>
2025-08-03 18:41:14 +0530

    fixed cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,29 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const authRoutes = require('./routes/authRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
 const analyticsRoutes = require('./routes/analyticsRoutes');
 
-
 const app = express();
-// CORS configuration
-const corsOptions = {
-  origin: ['http://localhost:5173', 'https://personal-finance-tracker-sand-tau.vercel.app'],
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
-
 app.use('/api/auth', authRoutes);
-
 app.use('/api/transactions', transactionRoutes);
-
 app.use('/api/analytics', analyticsRoutes);
-
 
 module.exports = app;
